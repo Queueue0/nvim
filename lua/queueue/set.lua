6fd6fff18abd0bde9c31f6b3787fc399785ac5c7
Queueue0 <jcoffey97@gmail.com>
2023-11-02 09:46:16 -0400

    Remove auto-format, add format bind

--- lua/queueue/set.lua
+++ lua/queueue/set.lua
@@ -24,9 +24,3 @@ vim.api.nvim_create_autocmd("VimEnter", {
         end
     end,
 })
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-    callback = function()
-        vim.lsp.buf.format()
-    end,
-})

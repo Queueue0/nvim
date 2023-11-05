727450624652872e207aefda0d0a5c240f8c17de
Queueue0 <jcoffey97@gmail.com>
2023-11-05 17:11:53 -0500

    Fixed mason language filter, added laptop binds to harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,3 +8,7 @@ vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-n>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-e>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-i>", function() ui.nav_file(4) end)
+-- Laptop keybinds
+vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,15 @@
+require("telescope").setup {
+    extensions = {
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown {}
+        }
+    }
+}
+
+require("telescope").load_extension("ui-select")
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
